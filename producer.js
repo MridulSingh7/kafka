18cc66449a307e70583b0b1e76bb714672f84db9
MridulSingh7 <mridulsingh425.personal@gmail.com>
2026-01-18 16:11:20 +0530

    .

---
+++ producer.js
@@ -0,0 +1,39 @@
+const { kafka } = require("./client");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function init() {
+  const producer = kafka.producer();
+
+  console.log("Connecting Producer");
+  await producer.connect();
+  console.log("Producer Connected Successfully");
+
+  rl.setPrompt("> ");
+  rl.prompt();
+
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
+  });
+}
+
+init();
+
+
+//this is when the producer sends a data to the broker, this code will just produce the data i.e send the data to the broker
